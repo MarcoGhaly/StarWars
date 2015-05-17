70f43e59e3b0cd3580e52f21d0f1c9bd8dc6552d
MarcoGhaly <Marco.gzy@gmail.com>
2015-05-17 15:29:19 +0300

    Bullets explosions

--- web/script/enemies/Asteroid.js
+++ web/script/enemies/Asteroid.js
@@ -3,28 +3,29 @@ var screenWidth = $(window).width();
 var screenHeight = $(window).height();
 
 // enemy units
-var asteroid_moveUnit = 7;
+var asteroid_moveUnit = 6;
 var asteroid_width = 75;
 var asteroid_height = 75;
 var asteroid_strength = 30;
 
 // alawys running functions
 setInterval(generateAsteroids, 1000);
-setInterval(updateAsteroids, 25);
+setInterval(updateAsteroids, 30);
 
 function generateAsteroids() {
     var asteroid = document.createElement('img');
     asteroid.setAttribute('src', 'img/asteroid.png');
+    asteroid.setAttribute('class', 'enemy');
     asteroid.setAttribute('name', 'asteroid');
-    asteroid.setAttribute('width', asteroid_width + 'px');
-    asteroid.setAttribute('height', asteroid_height + 'px');
+    asteroid.style.width = asteroid_width + 'px';
+    asteroid.style.height = asteroid_height + 'px';
     asteroid.style.position = 'absolute';
 
     if (parseInt(Math.random() * 3) === 0) {
         asteroid.style.left = -asteroid_width + 'px';
         asteroid.style.top = parseInt(Math.random() * (screenHeight / 2)) + 'px';
     } else {
-        asteroid.style.left = parseInt(Math.random() * (screenWidth / 2)) + 'px';
+        asteroid.style.left = parseInt(Math.random() * (screenWidth * 2 / 3)) + 'px';
         asteroid.style.top = -asteroid_height + 'px';
     }
     asteroid.strength = asteroid_strength;
