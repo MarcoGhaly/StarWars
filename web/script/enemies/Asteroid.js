6a39d619428f8c32b1d0daca7193760a20319eef
MarcoGhaly <Marco.gzy@gmail.com>
2015-05-17 12:07:58 +0300

    Explosions

--- web/script/enemies/Asteroid.js
+++ web/script/enemies/Asteroid.js
@@ -3,9 +3,10 @@ var screenWidth = $(window).width();
 var screenHeight = $(window).height();
 
 // enemy units
-var asteroid_moveUnit = 8;
-var asteroid_width = 100;
-var asteroid_height = 100;
+var asteroid_moveUnit = 7;
+var asteroid_width = 75;
+var asteroid_height = 75;
+var asteroid_strength = 30;
 
 // alawys running functions
 setInterval(generateAsteroids, 1000);
@@ -17,16 +18,16 @@ function generateAsteroids() {
     asteroid.setAttribute('name', 'asteroid');
     asteroid.setAttribute('width', asteroid_width + 'px');
     asteroid.setAttribute('height', asteroid_height + 'px');
-
     asteroid.style.position = 'absolute';
 
-    if (parseInt(Math.random() * 4) === 0) {
+    if (parseInt(Math.random() * 3) === 0) {
         asteroid.style.left = -asteroid_width + 'px';
-        asteroid.style.top = parseInt(Math.random() * (screenHeight / 3)) + 'px';
+        asteroid.style.top = parseInt(Math.random() * (screenHeight / 2)) + 'px';
     } else {
         asteroid.style.left = parseInt(Math.random() * (screenWidth / 2)) + 'px';
         asteroid.style.top = -asteroid_height + 'px';
     }
+    asteroid.strength = asteroid_strength;
     document.body.appendChild(asteroid);
 }
 
@@ -34,7 +35,7 @@ function updateAsteroids() {
     var asteroids = document.getElementsByName('asteroid');
     for (var i = 0; i < asteroids.length; i++) {
         asteroids[i].style.top = parseInt(asteroids[i].style.top) + asteroid_moveUnit + 'px';
-        asteroids[i].style.left = parseInt(asteroids[i].style.left) + asteroid_moveUnit + 'px';
+        asteroids[i].style.left = parseInt(asteroids[i].style.left) + asteroid_moveUnit * 1.5 + 'px';
         if (parseInt(asteroids[i].style.top) > screenHeight) {
             document.body.removeChild(asteroids[i]);
         }
